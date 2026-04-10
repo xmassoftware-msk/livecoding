5b2ec54b3dbc005f832d1f41e566c7a1460a288f
tortajet@yahoo.com <tortajet@yahoo.com>
2026-04-10 23:25:19 +0200

    Use modern Tidal 1.9+ BootTidal.hs with mkTidal API
    - Use Sound.Tidal.Boot import for new API
    - Use mkTidal instead of startStream
    - Add Tidally instance for boot aliases
    - Simpler, cleaner configuration

--- BootTidal.hs
+++ BootTidal.hs
@@ -1,108 +1,21 @@
-:set -XOverloadedStrings
+:set -fno-warn-orphans -Wno-type-defaults -XMultiParamTypeClasses -XOverloadedStrings
 :set prompt ""
 
-import Sound.Tidal.Context
+-- Import all the boot functions and aliases.
+import Sound.Tidal.Boot
 
-import System.IO (hSetEncoding, stdout, utf8)
-hSetEncoding stdout utf8
+default (Rational, Integer, Double, Pattern String)
 
-let editorTarget = Target {oName = "editor", oAddress = "127.0.0.1", oPort = 6013, oLatency = 0.02, oSchedule = Pre BundleStamp, oWindow = Nothing, oHandshake = False, oBusPort = Nothing }
-let editorShape = OSCContext "/editor/highlights"
+-- Create a Tidal Stream with the default settings.
+-- To customize these settings, use 'mkTidalWith' instead
+tidalInst <- mkTidal
 
-tidal <- startStream (defaultConfig {cFrameTimespan = 1/50}) [(superdirtTarget {oLatency = 0.2}, [superdirtShape]), (editorTarget, [editorShape])]
+-- This orphan instance makes the boot aliases work!
+-- It has to go after you define 'tidalInst'.
+instance Tidally where tidal = tidalInst
 
-:{
-let only = (hush >>)
-    p = streamReplace tidal
-    hush = streamHush tidal
-    panic = do hush
-               once $ sound "superpanic"
-    list = streamList tidal
-    mute = streamMute tidal
-    unmute = streamUnmute tidal
-    unmuteAll = streamUnmuteAll tidal
-    unsoloAll = streamUnsoloAll tidal
-    solo = streamSolo tidal
-    unsolo = streamUnsolo tidal
-    once = streamOnce tidal
-    first = streamFirst tidal
-    asap = once
-    nudgeAll = streamNudgeAll tidal
-    all = streamAll tidal
-    resetCycles = streamResetCycles tidal
-    setCycle = streamSetCycle tidal
-    setcps = asap . cps
-    getcps = streamGetcps tidal
-    getnow = streamGetnow tidal
-    xfade i = transition tidal True (Sound.Tidal.Transition.xfadeIn 4) i
-    xfadeIn i t = transition tidal True (Sound.Tidal.Transition.xfadeIn t) i
-    histpan i t = transition tidal True (Sound.Tidal.Transition.histpan t) i
-    wait i t = transition tidal True (Sound.Tidal.Transition.wait t) i
-    waitT i f t = transition tidal True (Sound.Tidal.Transition.waitT f t) i
-    jump i = transition tidal True (Sound.Tidal.Transition.jump) i
-    jumpIn i t = transition tidal True (Sound.Tidal.Transition.jumpIn t) i
-    jumpIn' i t = transition tidal True (Sound.Tidal.Transition.jumpIn' t) i
-    jumpMod i t = transition tidal True (Sound.Tidal.Transition.jumpMod t) i
-    jumpMod' i t p = transition tidal True (Sound.Tidal.Transition.jumpMod' t p) i
-    mortal i lifespan release = transition tidal True (Sound.Tidal.Transition.mortal lifespan release) i
-    interpolate i = transition tidal True (Sound.Tidal.Transition.interpolate) i
-    interpolateIn i t = transition tidal True (Sound.Tidal.Transition.interpolateIn t) i
-    clutch i = transition tidal True (Sound.Tidal.Transition.clutch) i
-    clutchIn i t = transition tidal True (Sound.Tidal.Transition.clutchIn t) i
-    anticipate i = transition tidal True (Sound.Tidal.Transition.anticipate) i
-    anticipateIn i t = transition tidal True (Sound.Tidal.Transition.anticipateIn t) i
-    forId i t = transition tidal False (Sound.Tidal.Transition.mortalOverlay t) i
-    d1 = p 1 . (|< orbit 0)
-    d2 = p 2 . (|< orbit 1)
-    d3 = p 3 . (|< orbit 2)
-    d4 = p 4 . (|< orbit 3)
-    d5 = p 5 . (|< orbit 4)
-    d6 = p 6 . (|< orbit 5)
-    d7 = p 7 . (|< orbit 6)
-    d8 = p 8 . (|< orbit 7)
-    d9 = p 9 . (|< orbit 8)
-    d10 = p 10 . (|< orbit 9)
-    d11 = p 11 . (|< orbit 10)
-    d12 = p 12 . (|< orbit 11)
-    d13 = p 13
-    d14 = p 14
-    d15 = p 15
-    d16 = p 16
-:}
-
-:{
-let getState = streamGet tidal
-    setI = streamSetI tidal
-    setF = streamSetF tidal
-    setS = streamSetS tidal
-    setR = streamSetR tidal
-    setB = streamSetB tidal
-:}
-
-:{
-let reverb = pF "reverb"
-    reverbsize = pF "reverbsize"
-    reverbdamp = pF "reverbdamp"
-    reverbpredelay = pF "reverbpredelay"
-    reverbwidth = pF "reverbwidth"
-    delay2 = pF "delay2"
-    delaytime = pF "delaytime"
-    delayfb = pF "delayfb"
-    delaypingpong = pF "delaypingpong"
-    sidechain = pF "sidechain"
-    scthreshold = pF "scthreshold"
-    scratio = pF "scratio"
-    scattack = pF "scattack"
-    screlease = pF "screlease"
-    clip = pF "clip"
-    clipthresh = pF "clipthresh"
-    clipmakeup = pF "clipmakeup"
-    lpf2 = pF "lpf2"
-    lpfcutoff = pF "lpfcutoff"
-    lpfreso = pF "lpfreso"
-:}
+-- You can also add your own aliases in this file. For example:
+-- fastsquizzed pat = fast 2 $ pat # squiz 1.5
 
 :set prompt "tidal> "
 :set prompt-cont ""
-
-default (Pattern String, Integer, Double)
